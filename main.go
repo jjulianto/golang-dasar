b2ef98da157848fc603ff6c1e38d084c2b57153f
Julianto08 <jjulianto08@gmail.com>
2021-04-05 18:56:32 +0700

    Add some further techniques in Golang

--- main.go
+++ main.go
@@ -79,4 +79,114 @@ func main() {
 			fmt.Println("index :", index, " letter :", string(letter))
 		}
 	}
+
+	//Array
+	var languages [5]string
+	languages[0] = "Go"
+	languages[1] = "Phyton"
+	languages[2] = "Java"
+	languages[3] = "C++"
+	languages[4] = "JavaScript"
+
+	fmt.Println(languages)
+	length := len(languages)
+	fmt.Println(length)
+
+	languagess := [...]string{
+		"Ruby",
+		"Go",
+		"Java",
+		"C#",
+		"JavaScript",
+		"C++",
+	}
+
+	for index, lang := range languagess {
+		fmt.Println("Index : ", index, " Language :", lang)
+	}
+
+	//Slice
+	var gamingConsoles []string
+	gamingConsoles = append(gamingConsoles, "Playstation 4")
+	gamingConsoles = append(gamingConsoles, "Nintendo Switch")
+	gamingConsoles = append(gamingConsoles, "Xbox One")
+
+	for _, console := range gamingConsoles {
+		fmt.Println(console)
+	}
+
+	//Map
+	var myMap map[string]int
+	myMap = map[string]int{}
+
+	myMap["Ruby"] = 9
+	myMap["Java"] = 7
+	myMap["Kotlin"] = 8
+
+	fmt.Println(myMap)
+	fmt.Println(myMap["Ruby"])
+
+	myMaps := map[string]string{
+		"Go": "Niceeee",
+		"C#": "Gooodd",
+	}
+
+	fmt.Println(myMaps)
+
+	mapMap := map[string]string{
+		"ruby":       "Very gooodd",
+		"go":         "goooodddd",
+		"javascript": "hype",
+	}
+
+	for key, value := range mapMap {
+		fmt.Println("Key : ", key, " value : ", value)
+	}
+
+	fmt.Println("==============")
+
+	delete(mapMap, "ruby")
+
+	for key, value := range mapMap {
+		fmt.Println("Key : ", key, " value : ", value)
+	}
+
+	value, isAvailable := mapMap["python"]
+	fmt.Println(isAvailable)
+	fmt.Println(value)
+
+	//Slice of Map
+	students := []map[string]string{
+		{"name": "Agus", "score": "A"},
+		{"name": "Nugroho", "score": "B"},
+		{"name": "Rahmawan", "score": "C"},
+	}
+
+	for _, student := range students {
+		fmt.Println(student["name"], " - ", student["score"])
+	}
+
+	//Quiz
+	scores := [8]int{100, 88, 75, 92, 70, 93, 85, 67}
+
+	var total int
+
+	for _, score := range scores {
+		total += score
+	}
+
+	lengthh := len(scores)
+	average := float64(total) / float64(lengthh)
+
+	fmt.Println(average)
+
+	var goodScores []int
+
+	for _, score := range scores {
+		if score >= 90 {
+			goodScores = append(goodScores, score)
+		}
+	}
+
+	fmt.Println(goodScores)
 }
